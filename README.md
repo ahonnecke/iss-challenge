822cc57e1ecd78ed899e492300cf94597d4a569d
Ashton Honnecke <ahonnecke@gmail.com>
2020-07-20 20:05:41 -0600

    finish

--- README.md
+++ README.md
@@ -24,3 +24,24 @@ for each craft print the details of those people that are currently in space
 
 When finished send me back your results! I am looking forward to hearing your
 thoughts.
+
+## Usage
+
+``` bash
+ahonnecke@homonym:~/Documents/professional/challenges/brooksource$ docker-compose run app loc
+The ISS currnet location at 1595297080 is 44.7975, -97.9406
+```
+
+``` bash
+ahonnecke@homonym:~/Documents/professional/challenges/brooksource$ docker-compose run app pass --lat=45 --lon=-122.3
+The ISS will be overhead 45.0, -122.3 at 1595296591 for 550
+```
+
+``` bash
+ahonnecke@homonym:~/Documents/professional/challenges/brooksource$ docker-compose run app people
+Chris Cassidy
+Anatoly Ivanishin
+Ivan Vagner
+Doug Hurley
+Bob Behnken
+```
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import argparse
+import requests
 
 LOC = "loc"
 PASS = "pass"
@@ -14,35 +15,79 @@ def get_args():
         help="Specify the type of check to perform",
         choices=[LOC, PASS, PEOPLE],
     )
-    parser.add_argument("lat", help="Specify the latitude", nargs="?")
-    parser.add_argument("long", help="Specify the longitude", nargs="?")
+    parser.add_argument("--lat", help="Specify the latitude")
+    parser.add_argument("--lon", help="Specify the longitude")
     return parser.parse_args()
 
 
+# {
+#     "message": "success",
+#     "timestamp": 1595296455,
+#     "iss_position": {"latitude": "18.2417", "longitude": "-132.0077"},
+# }
 def _loc():
     url = "/".join([BASE_API, "iss-now.json"])
-    print(url)
+    body = requests.get(url).json()
+    print(
+        f"The ISS currnet location at {body['timestamp']} is "
+        f"{body['iss_position']['latitude']}, {body['iss_position']['longitude']}"
+    )
 
 
+# {
+#     "message": "success",
+#     "request": {
+#         "altitude": 100,
+#         "datetime": 1595296427,
+#         "latitude": 45.0,
+#         "longitude": -122.3,
+#         "passes": 5,
+#     },
+#     "response": [
+#         {"duration": 550, "risetime": 1595296591},
+#         {"duration": 654, "risetime": 1595302318},
+#         {"duration": 632, "risetime": 1595308153},
+#         {"duration": 628, "risetime": 1595314000},
+#         {"duration": 654, "risetime": 1595319816},
+#     ],
+# }
 def _pass(lat, lon):
     url = "/".join([BASE_API, f"iss-pass.json?lat={lat}&lon={lon}"])
-    print(url)
+    body = requests.get(url).json()
+    print(
+        f"The ISS will be overhead",
+        f"{body['request']['latitude']}, {body['request']['longitude']}",
+        f"at {body['response'][0]['risetime']} for {body['response'][0]['duration']}",
+    )
 
 
+# {
+#     "message": "success",
+#     "number": 5,
+#     "people": [
+#         {"craft": "ISS", "name": "Chris Cassidy"},
+#         {"craft": "ISS", "name": "Anatoly Ivanishin"},
+#         {"craft": "ISS", "name": "Ivan Vagner"},
+#         {"craft": "ISS", "name": "Doug Hurley"},
+#         {"craft": "ISS", "name": "Bob Behnken"},
+#     ],
+# }
 def _people():
     url = "/".join([BASE_API, "astros.json"])
-    print(url)
+    body = requests.get(url).json()
+    for person in body["people"]:
+        print(person.get("name", "Ghostronaut"))
 
 
 def main(args):
     if args.type == LOC:
         return _loc()
-    elif args.type == PASS:
-        return _pass(45.0, -122.3)
-    elif args.type == PEOPLE:
+    if args.type == PASS:
+        return _pass(args.lat, args.lon)
+    if args.type == PEOPLE:
         return _people()
 
-    raise TypeError("Unsupported type passed in ")
+    raise TypeError(f"Unsupported type {args.type} passed in")
 
 
 if __name__ == "__main__":
